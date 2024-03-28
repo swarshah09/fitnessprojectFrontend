4bbae6c2a482b43c341333a6ba223f0d0c025d2f
Swar Shah <90791181+swarshah09@users.noreply.github.com>
2024-03-28 18:08:48 +0530

    Update AuthPopup.tsx

--- src/components/AuthPopup/AuthPopup.tsx
+++ src/components/AuthPopup/AuthPopup.tsx
@@ -127,6 +127,7 @@ const AuthPopup: React.FC<AuthPopupProps> = ({ setShowpopup }) => {
         console.log(loginformData);
 
         fetch(process.env.NEXT_PUBLIC_BACKEND_API + '/auth/login', {
+            mode: 'no-cors',
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
